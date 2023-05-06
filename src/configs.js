ea15d7cc28376b6c55f67a6d3fc6afb12bdd79c4
Amark19 <amarkhamkar6@gmail.com>
2023-05-06 21:00:22 +0530

    first commit

---
+++ src/configs.js
@@ -0,0 +1,158 @@
+import {
+  displayAt,
+  STOPS,
+  PAGE_DEPH_MARGIN,
+  SKILLS_LIST,
+  EXPERIENCE,
+} from './constants/pageData';
+
+const configs = {
+  title: 'Amar Khamkar',
+  subTitle: 'AmarDdev',
+  stars: {
+    maxSize: 20,
+    emissionIntencity: 40,
+    //optionally stars can be an image
+    // url: 'icons/three-js-logo.png',
+    dencity: 900,
+  },
+  navTitle: "Amar Khamkar's portfolio.",
+  relevantStops: STOPS,
+  pages: [
+    {
+      scale: 15,
+      rotation: [Math.PI, 0, Math.PI],
+      position: [0, 0, -PAGE_DEPH_MARGIN],
+      displayAt: displayAt('About'),
+      title: 'Who is Amar Khamkar ?',
+      infos: [
+        "- I am a problem solver who loves to utilize various technologies to develop software solutions.",
+        '- A computer engineer, started my development journey using Python and Django.',
+        '- Over the past four years, I have gained extensive skills in software engineering and have developed projects that have impacted over 300,000 users.',
+        '- Apart from software development, I am also a fitness enthusiast who enjoys hitting the gym every day.',
+      ],
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Experience'),
+      rotation: [0, -Math.PI / 2, 0],
+      position: [-PAGE_DEPH_MARGIN, 0, 0],
+      title: 'Experience',
+      imageList: EXPERIENCE,
+      footer: 'You can click the icons to find out more.',
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Skills'),
+      rotation: [-Math.PI / 2, 0, -Math.PI / 2],
+      position: [0, PAGE_DEPH_MARGIN, 0],
+      title: 'Skills',
+      footer: 'And more...',
+      imageList: SKILLS_LIST,
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Download CV'),
+      rotation: [0, 0, 0],
+      position: [0, 0, PAGE_DEPH_MARGIN],
+      title: 'Here is my CV!',
+      footer: 'Click the icon above to view & to open click on resume',
+      imageList: {
+        rows: 1,
+        separation: 3,
+        leftPadding: 0,
+        topPadding: 0,
+        scale: 0.8,
+        items: [
+          {
+            url: 'icons/resume.png',
+            imgUrl:
+              'pictures/resumes/my-resume.jpg',
+          },
+        ], imageViewer: {
+          position: [0, 0, -15],
+          rotation: [Math.PI, Math.PI, Math.PI],
+          height: 1024,
+          width: 1024,
+        },
+      },
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Personal Projects'),
+      rotation: [Math.PI / 2, 0, 0],
+      position: [0, -PAGE_DEPH_MARGIN, 0],
+      title: 'Projects',
+      footer: '(And many more...)',
+      imageList: {
+        rows: 2,
+        separation: 2.5,
+        leftPadding: -0.4,
+        topPadding: 0.2,
+        scale: 0.3,
+        items: [
+          {
+            url: 'websites-preview/calcont-logo.png',
+            websiteURL: 'https://www.calcont.in/',
+          },
+          {
+            url: 'websites-preview/safewrite-logo.png',
+            websiteURL: 'https://play.google.com/store/apps/details?id=com.ARmarstudio.notesapp',
+          },
+          {
+            url: 'websites-preview/monuverse-logo.png',
+            websiteURL: 'https://play.google.com/store/apps/details?id=com.aprendizstudio.monuverse&hl=en_IN&gl=US',
+          },
+          {
+            url: 'websites-preview/arlab-logo.png',
+            websiteURL: 'https://play.google.com/store/apps/details?id=com.aprendizstudio.ARLAB&hl=en_IN&gl=US&pli=1',
+          },
+          {
+            url: 'websites-preview/arsliding-logo.png',
+            websiteURL: 'https://play.google.com/store/apps/details?id=com.ARmarstudio.arslidingpuzzle',
+          },
+          {
+            url: 'websites-preview/calcont-blog-logo.png',
+            websiteURL: 'https://blog.calcont.in/',
+          },
+        ],
+      },
+    },
+    {
+      scale: 15,
+      displayAt: displayAt('Get In Touch'),
+      rotation: [0, Math.PI / 2, 0],
+      position: [PAGE_DEPH_MARGIN, 0, 0],
+      title: 'You can find me in:',
+      footer: 'Almost done but keep scrolling :)',
+      imageList: {
+        rows: 2,
+        separation: 1.8,
+        leftPadding: -0.25,
+        topPadding: 0.2,
+        scale: 0.3,
+        items: [
+          {
+            url: 'icons/linkedin.png',
+            websiteURL:
+              'https://www.linkedin.com/in/amar-khamkar-2710731a5/',
+          },
+          {
+            url: 'icons/github.png',
+            websiteURL: 'https://github.com/Amark19',
+          },
+          {
+            url: 'icons/email.png',
+            websiteURL: 'mailto:amarkhamkar6@gmail.com',
+          },
+          {
+            url: 'icons/twitter.png',
+            websiteURL: 'https://twitter.com/AmarKhamkar6',
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default configs;
---
+++ src/constants/pageData.js
@@ -0,0 +1,113 @@
+/* 
+  There are some weird things happening with the offset and the camera 
+  movement because of the implementation of the smooth stop of the camera.
+
+  My solution is to define two diferent positions.
+  position is relative to the scrollComponent, that's for the nav bar, 
+  displayPosition is the offset of the default camara position when facing the cube.
+*/
+
+export const PAGE_DEPH_MARGIN = 15;
+
+export const EXPERIENCE = {
+  rows: 1,
+  separation: 2,
+  leftPadding: -0.32,
+  topPadding: 0.22,
+  scale: 0.2,
+  //optionally show the websiteURL inside of the cube in an Iframe
+  webViewer: {
+    position: [0, 0, -15],
+    rotation: [Math.PI, Math.PI, Math.PI],
+    height: 720,
+    width: 1024,
+  },
+  items: [
+    {
+      url: 'websites-preview/hackerrank.png',
+      websiteURL: 'https://hackerrank.com/',
+    },
+    {
+      url: 'websites-preview/ajnalens.jpeg',
+      websiteURL: 'https://www.ajnalens.com/',
+    },
+  ],
+};
+
+export const SKILLS_LIST = {
+  rows: 4,
+  separation: 2.4,
+  leftPadding: -0.4,
+  topPadding: 0.33,
+  scale: 0.15,
+  items: [
+    { url: 'icons/python-logo.png', websiteURL: null },
+    { url: 'icons/csharp-logo.png', websiteURL: null },
+    { url: 'icons/django-logo.png', websiteURL: null },
+    { url: 'icons/unity-logo.png', websiteURL: null },
+    { url: 'icons/js-logo.png', websiteURL: null },
+    { url: 'icons/flutter-logo.png', websiteURL: null },
+    { url: 'icons/flask-logo.png', websiteURL: null },
+    { url: 'icons/react-logo.png', websiteURL: null },
+    { url: 'icons/node-js-logo.png', websiteURL: null },
+    { url: 'icons/tensorflow-logo.png', websiteURL: null },
+    { url: 'icons/firebase-logo.png', websiteURL: null },
+    { url: 'icons/nginx-logo.png', websiteURL: null },
+  ],
+};
+
+export const STOPS = [
+  { displayPosition: 0, position: 0, name: 'Title' },
+  {
+    position: 0.13,
+    displayPosition: 0.13,
+    displayPositionOffset: -0.04,
+    displayPositionOffsetEnd: -0.02,
+    name: 'About',
+  },
+  {
+    position: 0.2137,
+    displayPosition: 0.2007,
+    displayPositionOffset: -0.03,
+    displayPositionOffsetEnd: -0.07,
+    name: 'Experience',
+  },
+  {
+    position: 0.2982,
+    displayPosition: 0.232,
+    displayPositionOffset: 0.05,
+    displayPositionOffsetEnd: -0.12,
+    name: 'Skills',
+  },
+  {
+    position: 0.3899,
+    displayPosition: 0.3009,
+    displayPositionOffset: 0.08,
+    displayPositionOffsetEnd: -0.17,
+    name: 'Download CV',
+  },
+  {
+    position: 0.48,
+    displayPosition: 0.29,
+    displayPositionOffset: 0.18,
+    displayPositionOffsetEnd: -0.15,
+    name: 'Personal Projects',
+  },
+  {
+    position: 0.592,
+    displayPosition: 0.28,
+    displayPositionOffset: 0.3,
+    displayPositionOffsetEnd: -0.1,
+    name: 'Get In Touch',
+  },
+  {
+    position: 1,
+    displayPosition: 1,
+    displayPositionOffset: 0.03,
+    displayPositionOffsetEnd: 0,
+    name: 'Profile Picture',
+  },
+];
+
+export const displayAt = (stopName) =>
+  STOPS.find((stop) => stop.name === stopName);
